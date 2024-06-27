496b16449df8edf9c78a28227d1d7812d9b08a89
kuswanthkumar7 <kuswanthregidi@gmail.com>
2024-06-27 09:45:49 +0530

    codeforces problems

---
+++ watermelon.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	int w;
+	scanf("%d",&w);
+	if(w%2!=0 || w==2){
+		printf("no");
+	else{
+		int x=w/2;
+		if(x%2==0){
+			printf("%d %d",x,x);
+		}
+		else{
+			printf("%d %d",x-1,x+1);
+	    	}
+    	}
+	}
+}
